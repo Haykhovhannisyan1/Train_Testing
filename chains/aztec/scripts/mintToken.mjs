b3d320c09582e4893a53b635742f8d65dc52a1ed
nerses-asaturyan <nerses.asaturyan@gmail.com>
2025-04-10 18:56:29 +0400

    feat(aztec) add contract deployment script

--- chains/aztec/scripts/mintToken.mjs
+++ chains/aztec/scripts/mintToken.mjs
@@ -1,7 +1,7 @@
 import { getInitialTestAccountsWallets } from '@aztec/accounts/testing';
 import { Contract, createPXEClient, loadContractArtifact, waitForPXE } from '@aztec/aztec.js';
 import TokenContractJson from "../contracts/token/target/token-Token.json" assert { type: "json" };
-import { writeFileSync } from 'fs';
+import { updateData } from './utils.mjs';
 
 const TokenContractArtifact = loadContractArtifact(TokenContractJson);
 const { PXE_URL = 'http://localhost:8080' } = process.env;
@@ -11,26 +11,35 @@ async function main() {
   const pxe = createPXEClient(PXE_URL);
   await waitForPXE(pxe);
 
-  const [ownerWallet] = await getInitialTestAccountsWallets(pxe);
-  const ownerAddress = ownerWallet.getAddress();
-  console.log(`Using wallet: ${ownerAddress}`);
+  const [recipientWallet , deployerWallet] = await getInitialTestAccountsWallets(pxe);
+  const deployerAddress = deployerWallet .getAddress();
+  console.log(`Using wallet: ${deployerAddress}`);
 
   console.log("Deploying token contract...");
-  const token = await Contract.deploy(ownerWallet, TokenContractArtifact, [ownerAddress, 'TRAIN', 'TRN', 18])
+  const token = await Contract.deploy(deployerWallet , TokenContractArtifact, [deployerAddress, 'TRAIN', 'TRN', 18])
     .send()
     .deployed();
   console.log(`Token deployed at ${token.address.toString()}`);
 
-  const amount = 1234n;
+  const amount = 9876543210n;
   console.log(`Minting ${amount} tokens...`);
-  const contract = await Contract.at(token.address, TokenContractArtifact, ownerWallet);
-  const mintTx = await contract.methods.mint_to_public(ownerAddress, amount).send().wait();
-  console.log(`Mint successful in block ${mintTx.blockNumber}`);
+  const contract = await Contract.at(token.address, TokenContractArtifact, deployerWallet );
+  const mintTx = await contract.methods.mint_to_public(deployerAddress, amount).send().wait();
+  console.log(`Public mint successful in block ${mintTx.blockNumber}`);
 
-  const balanceResult = await contract.methods.balance_of_public(ownerAddress).simulate();
-  console.log(`Balance of ${ownerAddress} : ${balanceResult}`);
-  const tokenData = { token: token.address.toString(), owner: ownerAddress.toString() };
-  writeFileSync('tokenData.json', JSON.stringify(tokenData, null, 2));
+  const balanceResult = await contract.methods.balance_of_public(deployerAddress).simulate();
+  console.log(`Balance of ${deployerAddress} : ${balanceResult}`);
+
+  const mintTx2 = await contract.methods.transfer_to_private(recipientWallet.getAddress(), amount/2n).send().wait();
+  console.log(`Private transfer successful in block ${mintTx2.blockNumber}`);
+
+  const balanceResult2 = await contract.methods.balance_of_private(recipientWallet.getAddress()).simulate();
+  console.log(`Balance of ${recipientWallet.getAddress()} : ${balanceResult2}`);
+
+  updateData({
+    token: token.address.toString(),
+    tokenOwner: deployerWallet.getAddress(),
+});
 }
 
 main().catch((err) => {
